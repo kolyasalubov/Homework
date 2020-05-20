1c90d129f543ed633dfbb0988fea19ffc63800e8
Hellybery <oliakapustynska@gmail.com>
2020-05-20 10:16:59 +0300

    Add new file HW_1_Task_1

---
+++ HW_1_Task_1.py
@@ -0,0 +1,23 @@
+###   task_1_a   ###
+
+a = int(input('Input the number: '))
+b = int(input('Input the number: '))
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+###   task_1_b   ###
+
+a = int(input('Input number a: '))
+b = int(input('Input number b: '))
+print('a + b =', a + b)
+print('a - b =', a - b)
+print('a * b =', a * b)
+print('a / b =', a - b)
+
+###   task_1_c   ###
+
+a = int(input('Input the number: '))
+b = int(input('Input the number: '))
+print(a + b, a - b, a * b, a - b)
